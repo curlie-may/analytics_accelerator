fb5e894345854cbf2ecf7802e97eb80ed7d5ce18
Lucymarie <mantese@msn.com>
2025-08-21 19:50:59 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -225,4 +225,17 @@ FROM accounts
 WHERE (name LIKE 'C%' OR name LIKE 'W%') 
 AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE 'Ana%') AND primary_poc NOT LIKE '%eana%');
 
+--Q3.4 Try pulling all the data from the accounts table, and all the data from the orders table.
+SELECT orders.*, accounts.*
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id;
+
+--Q3.4 Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website and the primary_poc from the accounts table.
+SELECT orders.standard_qty, orders.gloss_qty, 
+          orders.poster_qty,  accounts.website, 
+          accounts.primary_poc
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id
 
