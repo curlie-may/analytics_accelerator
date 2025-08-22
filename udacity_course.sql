39b1ddc1e11ed7bd1ca70099598d62b059d4db77
Lucymarie <mantese@msn.com>
2025-08-21 20:20:52 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -237,5 +237,18 @@ SELECT orders.standard_qty, orders.gloss_qty,
           accounts.primary_poc
 FROM orders
 JOIN accounts
-ON orders.account_id = accounts.id
+ON orders.account_id = accounts.id;
+
+--Example JOIN MORE THAN ONE table
+SELECT *
+FROM web_events
+JOIN accounts
+ON web_events.account_id = accounts.id
+JOIN orders
+ON accounts.id = orders.account_id
+
+
+
+
+
 
