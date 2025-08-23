f056e1ad91fe74a69b925b7b84ba51f4cbbd5579
Lucymarie <mantese@msn.com>
2025-08-23 14:05:05 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -306,5 +306,34 @@ FROM orders;
 SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS standard_price_per_unit
 FROM orders;
 
+--Q4.11 When was the earliest order ever placed? You only need to return the date.
+SELECT MIN(occurred_at)
+FROM orders
+LIMIT 1;
+
+--Q4.11 Try performing the same query as in question 1 without using an aggregation function. 
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at;
+
+--Q4.11 When did the most recent (latest) web_event occur?
+SELECT MAX(occurred_at)
+FROM orders;
+
+--Q4.11 Try to perform the result of the previous query without using an aggregation function.
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+--Q4.11 Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. 
+--Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+SELECT AVG(standard_qty) AS avg_stnd, 
+       AVG(poster_qty) AS avg_poster,                  
+       AVG(gloss_qty) AS avg_gloss 
+FROM orders;
+
+--Q4.11 Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have covered so 
+--far try finding - what is the MEDIAN total_usd spent on all orders?
 
 
