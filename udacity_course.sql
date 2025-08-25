4ae9f9ba214ae427c08f90b830a8947b1f02891b
Lucymarie <mantese@msn.com>
2025-08-24 23:50:07 -0700

    Update udacity_course.sql

--- udacity_course.sql
+++ udacity_course.sql
@@ -337,3 +337,66 @@ FROM orders;
 --far try finding - what is the MEDIAN total_usd spent on all orders?
 
 
+--Q4.14 #1  Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+SELECT account_id, occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1;
+
+--Q4.14 #2 Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders 
+--in usd and the company name. 
+SELECT SUM(total), account_id 
+FROM orders
+GROUP BY account_id;
+
+--Q4.14 #3 Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? 
+--Your query should return only three values - the date, channel, and account name.
+SELECT occurred_at, channel, account_id 
+FROM web_events
+ORDER BY channel DESC
+LIMIT 1;
+
+--Q4.14 #4 Find the total number of times each type of channel from the web_events was used. Your final table should have 
+--two columns - the channel and the number of times the channel was used.
+SELECT channel, COUNT(channel) AS channel_count
+FROM web_events
+GROUP BY channel; 
+--#########  OR another solution:
+SELECT w.channel, COUNT(*)
+FROM web_events w
+GROUP BY w.channel
+
+--Q4.14 #5 Who was the primary contact associated with the earliest web_event?
+SELECT a.primary_poc
+FROM web_events AS w
+JOIN accounts AS a
+ON w.account_id = a.id
+ORDER BY w.occurred_at
+LIMIT 1;
+
+--Q4.14 #6 What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name 
+--and the total usd. Order from smallest dollar amounts to largest.
+SELECT a.name, MIN(o.total_amt_usd) as smallest_order
+FROM accounts AS a
+JOIN orders AS o
+ON a.id = o.account_id
+GROUP BY a.name 
+ORDER BY smallest_order;
+--OR since "name" and "total_amt_used', show up in only one table, 
+--it's ok to omit the table name such as:  
+SELECT name, MIN(total_amt_usd) as smallest_order
+FROM accounts AS a
+JOIN orders AS o
+ON a.id = o.account_id
+GROUP BY a.name 
+ORDER BY smallest_order;
+
+--Q4.14 #7 Find the number of sales reps in each region. Your final table should have two columns - the region and 
+--the number of sales_reps. Order from fewest reps to most reps.
+SELECT r.name, SUM(sales_rep_id) AS reps_total
+FROM accounts AS a
+JOIN sales_reps AS r
+ON r.id = a.sales_rep_id
+GROUP BY r.name
+ORDER BY reps_total;
+
