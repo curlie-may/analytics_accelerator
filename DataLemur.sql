ff64da71bc56c741e156477198972ec9f6055f02
Lucymarie <mantese@msn.com>
2025-09-15 10:44:06 -0700

    Update DataLemur.sql

--- DataLemur.sql
+++ DataLemur.sql
@@ -28,11 +28,28 @@ ORDER BY candidate_id;
 
 
 --Histogram of Tweets: Twitter SQL Interview Question
-SELECT t1.tweet_bucket, count(t1.user_id) AS user_num
+SELECT t1.tweet_bucket, COUNT(t1.user_id) AS user_num
 FROM (
 SELECT COUNT(tweet_id) AS tweet_bucket, user_id
 FROM tweets
 WHERE DATE_TRUNC('year', tweet_date) = '2022-01-01'
 GROUP BY user_id
-) as t1
+) AS t1
 GROUP BY t1.tweet_bucket
+
+--Unfinished Parts: Tesla SQL Interview Question
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date IS Null AND assembly_step != 1
+
+--Teams Power Users Microsoft SQL Interview Question
+select  sender_id, count(content) as number_of_messages
+from messages
+where extract (year from sent_date) = '2022' and extract (month from sent_date) = '08' 
+group by sender_id
+order by number_of_messages DESC
+limit 2;
+
+
+
+
